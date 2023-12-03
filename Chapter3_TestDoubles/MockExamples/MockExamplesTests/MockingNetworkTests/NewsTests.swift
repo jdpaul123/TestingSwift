4801bec6889f286106777607e6d31afa0bde59e0
JD Paul <jdpaul123@gmail.com>
2023-12-02 20:53:21 -0800

    Cleaned up some code

--- Chapter3_TestDoubles/MockExamples/MockExamplesTests/MockingNetworkTests/NewsTests.swift
+++ Chapter3_TestDoubles/MockExamples/MockExamplesTests/MockingNetworkTests/NewsTests.swift
@@ -8,6 +8,13 @@
 import XCTest
 @testable import MockExamples
 
+/*
+ What we want to test:
+ 1. What was the URL that was requested?
+ 2. Was a network request actually started?
+ 3. Did the request come back with certain data?
+ 4. Did the request come back with a specific error?
+ */
 final class NewsTests: XCTestCase {
     func testNewsFetchLoadsCorrectURL() {
         class DataTaskMock: URLSessionDataTask {
@@ -72,7 +79,6 @@ final class NewsTests: XCTestCase {
                 return newDataTask
             }
         }
-
 
         // given
         let url = URL(string: "https://www.apple.com/newsroom/rss-feed.rss")!
@@ -157,60 +163,3 @@ final class NewsTests: XCTestCase {
         }
     }
 }
-
-
-//// MARK: These classes can be used to check that we load the correct url and that resume was called
-//// To test that the URL was requested we must make teh dataTask() method do nothing except save the URL that was sent
-//class DataTaskMock: URLSessionDataTask {
-//    let completionHandler: (Data?, URLResponse?, Error?) -> Void
-//    var resumeWasCalled = false
-//
-//
-//    init(completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
-//        self.completionHandler = completionHandler
-//    }
-//
-//    // This is an example of a dummy because it doesn't do anything
-//    override func resume() {
-//        // resume was called, so flip our boolean and call the completion
-//        resumeWasCalled = true
-//        completionHandler(nil, nil, nil)
-//    }
-//}
-//
-//class URLSessionMock: URLSessionProtocol {
-//    var dataTask: DataTaskMock?
-//    var lastURL: URL?
-//    var testData: Data?
-//
-//    func dataTask(with url: URL, completionHandler: @escaping DataTaskResult) -> URLSessionDataTask {
-//        // For checking that we load the correct url
-//        lastURL = url
-//
-//        // For checking that resume was called
-//        let newDataTask = DataTaskMock(completionHandler: completionHandler)
-//        dataTask = newDataTask
-//
-//        return newDataTask
-////        defer { completionHandler(nil, nil, nil)}
-////        lastURL = url
-////        return DataTaskMock() // TODO: The public init was deprecated for iOS 13.0 and newer. There is no accessable init
-//    }
-//}
-
-//// MARK: These calsses can be used to check that meaningful data or an error was returned
-//class DataTaskMock: URLSessionDataTask {
-//    override func resume() { }
-//}
-//
-//class URLSessionMock: URLSessionProtocol {
-//    var testData: Data?
-//    var error: Error?
-//
-//    func dataTask(with url: URL, completionHandler: @escaping DataTaskResult) -> URLSessionDataTask {
-//        defer {
-//            completionHandler(testData, nil, error)
-//        }
-//        return DataTaskMock()
-//    }
-//}
