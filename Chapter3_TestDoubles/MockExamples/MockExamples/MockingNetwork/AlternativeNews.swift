95cb85bdbfa26800057020feefef40a48d92f15c
JD Paul <jdpaul123@gmail.com>
2023-12-03 15:58:01 -0800

    Finished "Mocking networking: an alternative" and Chapter 3: Testing Swift

---
+++ Chapter3_TestDoubles/MockExamples/MockExamples/MockingNetwork/AlternativeNews.swift
@@ -0,0 +1,38 @@
+//
+//  AlternativeNews.swift
+//  MockExamples
+//
+//  Created by Jonathan Paul on 12/3/23.
+//
+
+import Foundation
+
+/*
+ What we want to test:
+ 1. What was the URL that was requested?
+ 2. Was a network request actually started?
+ 3. Did the request come back with certain data?
+ 4. Did the request come back with a specific error?
+ */
+class AlternativeNews {
+    var url: URL
+    var stories = ""
+
+    init(url: URL) {
+        self.url = url
+    }
+
+    // Pull out the hidden dependency URLSession.shared by making it the "session" parameter
+    // Make that session of type URLSessionProtocol rather than URLSession
+    func fetch(using session: URLSession = .shared, completionHandler: @escaping () -> Void) {
+        let task = session.dataTask(with: url) { data, response, error in
+            if let data = data {
+                self.stories = String(decoding: data, as: UTF8.self)
+            }
+
+            completionHandler()
+        }
+
+        task.resume()
+    }
+}
---
+++ Chapter3_TestDoubles/MockExamples/MockExamplesTests/MockingNetworkTests/NewsTestsAlternativeMethod.swift
@@ -0,0 +1,62 @@
+//
+//  NewsTestsAlternativeMethod.swift
+//  MockExamplesTests
+//
+//  Created by Jonathan Paul on 12/3/23.
+//
+// NOTE: No need to write any URLSession mock using this method. We configure our URLProtocolMock class with whatever test data we expect
+// to receive back, then ask URLSession to use that class as part of its networking
+
+import XCTest
+@testable import MockExamples
+
+class URLProtocolMock: URLProtocol {
+    // this is the data we're expecting to send back.
+    // For every url tested we save it's data in this testURLs dictionary.
+    static var testURLs = [URL: Data]()
+
+    // It can handle all kinds of URLRequests so we will return true here
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+
+    // We don't care what canonical means so we just send back whatever was sent in
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+
+    // As soon as loading starts, sned back our test data or an empty Data instance, then end loading immediately
+    override func startLoading() {
+        if let url = request.url {
+            self.client?.urlProtocol(self, didLoad: URLProtocolMock.testURLs[url] ?? Data())
+            self.client?.urlProtocolDidFinishLoading(self)
+        }
+    }
+
+    // We don't need this but it's required, so we'll just write an empty method
+    override func stopLoading() { }
+}
+
+final class NewsTestsAlternativeMethod: XCTestCase {
+    func testNewsStoriesAreFetched() {
+        // given
+        let url = URL(string: "https://www.apple.com/newsroom/rssfeed/rss")!
+        let news = AlternativeNews(url: url)
+        let expectation = XCTestExpectation(description: "Downloading news stories triggers resume().")
+        URLProtocolMock.testURLs = [url: Data("Hello, world!".utf8)]
+
+        // No cacheing using ephemeral
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [URLProtocolMock.self]
+        let session = URLSession(configuration: config)
+
+        // when
+        news.fetch(using: session) {
+            XCTAssertEqual(news.stories, "Hello, world!")
+            expectation.fulfill()
+        }
+
+        // then
+        wait(for: [expectation], timeout: 1)
+    }
+}
